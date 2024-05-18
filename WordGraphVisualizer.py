e924685f275579f21fc5d6fa2f4436ed45f561c2
Harlan Howe <harlan.howe@kinkaid.org>
2024-05-18 15:22:17 -0500

    created initializer

--- WordGraphVisualizer.py
+++ WordGraphVisualizer.py
@@ -1,1 +1,13 @@
+import threading
 
+from WordGraph import WordGraph
+import numpy as np
+
+CANVAS_SIZE = 800
+
+class WordGraphVisualizer:
+
+    def __init__(self, graph: WordGraph):
+        self.graph = WordGraph
+        self.canvas = np.zeros(shape=(CANVAS_SIZE, CANVAS_SIZE, 3), dtype=float)
+        self.canvas_lock = threading.Lock()
