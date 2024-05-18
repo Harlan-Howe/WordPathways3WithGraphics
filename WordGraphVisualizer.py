ef8a63553fd5fd0b5fb55dcf28e414f8820bf7e1
Harlan Howe <harlan.howe@kinkaid.org>
2024-05-18 16:33:22 -0500

    Added code for moving the active nodes and edges (copied from WordPathwaysWithGraphics project)... not yet tested.

--- WordGraphVisualizer.py
+++ WordGraphVisualizer.py
@@ -1,13 +1,23 @@
+import math
 import random
 import threading
-from typing import List
+from typing import List, Tuple
 
 import cv2
 
+from WordEdge import WordEdge
 from WordGraph import WordGraph
 import numpy as np
 
 CANVAS_SIZE = 800
+NEUTRAL_RADIUS = 40
+EDGE_ATTRACTION_FACTOR = 0.00075
+EDGE_REPULSION_MODIFIER = 0
+MUTUAL_REPULSION_FACTOR = 0.5
+BORDER_FORCE = 15
+BORDER_RANGE = 10
+max_effective_d_for_edges = 100.0
+min_movement = 0.0125
 
 class WordGraphVisualizer:
 
@@ -18,12 +28,13 @@ class WordGraphVisualizer:
         self.canvas_lock = threading.Lock()
         self.needs_update: bool = True
         self.word_locs: List[List[float]] = []
+        self.dirty_canvas: bool = True
 
         for id in range(len(self.graph.vertices)):
             self.word_locs.append([random.randint(10, CANVAS_SIZE-10), random.randint(10, CANVAS_SIZE-10)])
         print("WGV initialized.")
 
-    def draw_self(self):
+    def draw_graph(self):
         self.canvas_lock.acquire()
         self.canvas = np.zeros(shape=(CANVAS_SIZE, CANVAS_SIZE, 3), dtype=float)
 
@@ -42,3 +53,81 @@ class WordGraphVisualizer:
                             color=self.graph.vertices[word_id].color)
 
         self.canvas_lock.release()
+        self.dirty_canvas = True
+
+    def find_net_forces(self):
+        self.active_words: List[int] = []
+        self.active_words.extend(self.graph.visited)
+        for fd in self.graph.frontier:
+            self.active_words.append(fd.word_id)
+        self.net_forces:List[List[float]] = [[0, 0] for i in range(len(self.active_words))]
+
+        for word_id in range(len(self.active_words)):
+            for edge in self.graph.edges:
+                if edge.u == word_id or edge.v == word_id:
+                    F = self.force_from_edge(edge, attraction_factor=0.00075, repulsion_multiplier=-3, forward=True)
+                    if edge.u == word_id:
+                        self.net_forces[word_id][0] += F[0]
+                        self.net_forces[word_id][1] += F[1]
+                    else:
+                        self.net_forces[word_id][0] -= F[0]
+                        self.net_forces[word_id][1] -= F[1]
+            for word_id2 in range(word_id):
+                dx = self.word_locs[word_id][0] - self.word_locs[word_id2][0]
+                dy = self.word_locs[word_id][1] - self.word_locs[word_id2][1]
+                d_squared = math.pow(dx, 2) + math.pow(dy, 2)
+                if d_squared > NEUTRAL_RADIUS:
+                    self.net_forces[word_id][0] += dx / d_squared * MUTUAL_REPULSION_FACTOR
+                    self.net_forces[word_id][1] += dy / d_squared * MUTUAL_REPULSION_FACTOR
+                    self.net_forces[word_id2][0] -= dx / d_squared * MUTUAL_REPULSION_FACTOR
+                    self.net_forces[word_id2][1] -= dy / d_squared * MUTUAL_REPULSION_FACTOR
+
+            self.net_forces[word_id][0] += BORDER_FORCE * math.exp(-self.word_locs[word_id][0] / BORDER_RANGE)
+            self.net_forces[word_id][1] += BORDER_FORCE * math.exp(-self.word_locs[word_id][1] / BORDER_RANGE)
+            self.net_forces[word_id][0] -= BORDER_FORCE * math.exp(
+                -(CANVAS_SIZE - self.word_locs[word_id][0]) / BORDER_RANGE)
+            self.net_forces[word_id][1] -= BORDER_FORCE * math.exp(
+                -(CANVAS_SIZE - self.word_locs[word_id][1]) / BORDER_RANGE)
+
+    def force_from_edge(self, edge: WordEdge,
+                        attraction_factor: float,
+                        repulsion_multiplier: float,
+                        forward: bool) -> Tuple[float, float]:
+        u: List[float] = self.word_locs[edge.u]
+        v: List[float] = self.word_locs[edge.v]
+
+        dx = u[0] - v[0]
+        dy = u[1] - v[1]
+
+        d = min(max_effective_d_for_edges, math.sqrt(math.pow(dx, 2) + math.pow(dy, 2)))
+        F_mag = attraction_factor * math.pow(d - NEUTRAL_RADIUS, 2)
+        if d < NEUTRAL_RADIUS:
+            F_mag *= repulsion_multiplier
+
+        fx = -F_mag * dx / d
+        fy = -F_mag * dy / d
+
+        if not forward:
+            fx *= -1
+            fy *= -1
+
+        return fx, fy
+
+    def update_locations_from_forces(self) -> bool:
+        madeAChange = False
+        for word_id in range(len(self.active_words)):
+            self.word_locs[word_id][0] += self.net_forces[word_id][0]
+            self.word_locs[word_id][1] += self.net_forces[word_id][1]
+            if (self.net_forces[word_id][0] > min_movement or self.net_forces[word_id][1] > min_movement):
+                madeAChange = True
+
+        return madeAChange
+
+    def update_loop(self):
+        while True:
+            self.find_net_forces()
+            moved = self.update_locations_from_forces()
+            if moved:
+                self.draw_graph()
+            cv2.waitKey(1)
+
