ce4444fc992712bde7302c31248d2716267f472e
Harlan Howe <harlan.howe@kinkaid.org>
2024-05-18 17:16:18 -0500

    changed frontier data from keeping a list of edge ids to a list of word ids

--- FrontierData.py
+++ FrontierData.py
@@ -5,5 +5,5 @@ class FrontierData:
 
     def __init__(self, word_id: int, word_ids_to_here: List[int]):
         self.word_id = word_id
-        self.word_ids_to_here: word_ids_to_here
+        self.word_ids_to_here = word_ids_to_here
 
